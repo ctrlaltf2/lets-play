a209b7e163cb75041527a494e6f9629e48c2480e
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2018-06-01 21:39:16 -0400

    [Server] Implement turn logic (no scheduler needed)

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,14 +12,14 @@ int main(int argc, char** argv) {
     // std::thread runner(EmulatorController, argv[1], argv[2]);
     // runner.join();
     // std::thread(SNESController, argv[1], argv[2]);
-    // LetsPlayServer server;
-    // server.Run(std::stoul(std::string(argv[1])));
+    LetsPlayServer server;
+    server.Run(std::stoul(std::string(argv[1])));
     // std::cout << "Server >>didn't<< crash while shutting down" << '\n';
-    Scheduler s;
-    std::function<void()> one = []() { std::cout << "one" << '\n'; };
-    std::function<void()> two = []() { std::cout << "two" << '\n'; };
-    s.Schedule(one, std::chrono::seconds(1));
-    s.Schedule(two, std::chrono::seconds(2));
+    // Scheduler s;
+    // std::function<void()> one = []() { std::cout << "one" << '\n'; };
+    // std::function<void()> two = []() { std::cout << "two" << '\n'; };
+    // s.Schedule(one, std::chrono::seconds(1));
+    // s.Schedule(two, std::chrono::seconds(2));
 
     while (true)
         std::this_thread::sleep_until(std::chrono::steady_clock::now() +
