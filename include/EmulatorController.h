49a756483e6e69ee19a96ca1c646de30a3ae03b2
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2018-06-03 23:05:02 -0400

    Initial

--- include/EmulatorController.h
+++ include/EmulatorController.h
@@ -1,13 +1,31 @@
 #pragma once
+#include <cstdint>
+#include <iostream>
+
+#include "RetroCore.h"
 
 /*
- * Function to be run in a thread. Starts up a libretro core and reads a rom for
- * it. Once that's done, it links with the server parameter to do things such as
- * send screen updates or respond to input sends
- * @param corePath The path to the libretro core that should be loaded. NOTE: On
- * linux, the path must be either relative (./filename.so) or absolute
- * (/usr/lib/retroarch/core.so), filenames do not work
- * @param romPath The path to the rom that should be loaded and started. Same
- * filename rules apply as above.
+ * 10 Reasons for creating this pseudo class
+ *
+ * 1. C sucks
+ * 2. C sucks
+ * 3. C sucks
+ * 4. C sucks
+ * 5. C sucks
+ * 6. C sucks
+ * 7. C sucks
+ * 8. C sucks
+ * 9. C sucks
+ * 10. static class functions couldn't be registered as callbacks
  */
-void EmulatorController(const char* corePath, const char* romPath);
+namespace EmulatorController {
+void Run(const char* corePath, const char* romPath);
+bool OnEnvironment(unsigned cmd, void* data);
+void OnVideoRefresh(const void* data, unsigned width, unsigned height,
+                    size_t stride);
+void OnPollInput();
+std::int16_t OnGetInputState(unsigned port, unsigned device, unsigned index,
+                             unsigned id);
+void OnLRAudioSample(std::int16_t left, std::int16_t right);
+size_t OnBatchAudioSample(const std::int16_t* data, size_t frames);
+}  // namespace EmulatorController
