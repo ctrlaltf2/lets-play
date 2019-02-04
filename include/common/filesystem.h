2c5f9b0f5ca0afb1c43788b3bb8f65e5e7738c53
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2019-02-04 15:59:15 -0500

    Fix namespace build error Weird how this built on Termux but not anything else.

--- include/common/filesystem.h
+++ include/common/filesystem.h
@@ -1,7 +1,16 @@
+#pragma once
 #ifdef USE_BOOST_FILESYSTEM
-#include "boost/filesystem.hpp"
-namespace lib::filesystem = boost;
+    #include "boost/filesystem.hpp"
 #else
-#include <filesystem>
-namespace lib::filesystem = std::filesystem;
+    #include <filesystem>
 #endif
+
+namespace lib {
+
+#ifdef USE_BOOST_FILESYSTEM
+    namespace filesystem = boost::filesystem;
+#else
+    namespace filesystem = std::filesystem;
+#endif
+
+}
