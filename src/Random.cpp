35bf938fe90e4f1d78279810b9f81b48821ba678
ctrlaltf2 <calm.wheel7586@fastmail.com>
2024-05-28 21:05:51 -0400

    rm -rf ./*
    :')

--- src/Random.cpp
+++
@@ -1,8 +0,0 @@
-#include "Random.h"
-
-namespace rnd {
-    std::uint_fast32_t nextInt() {
-        static thread_local std::mt19937 gen(std::chrono::system_clock::now().time_since_epoch().count());
-        return gen();
-    }
-}
