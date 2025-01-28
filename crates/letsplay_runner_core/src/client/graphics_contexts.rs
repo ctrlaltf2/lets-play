b64eea9b6fe6b94ff833c9efb84dc594cf7e716c
modeco80 <lily.modeco80@protonmail.ch>
2025-01-27 22:02:38 -0500

    letsplay_runner_core: Add CudaGL interop resource to GraphicsContexts

--- crates/letsplay_runner_core/src/client/graphics_contexts.rs
+++ crates/letsplay_runner_core/src/client/graphics_contexts.rs
@@ -3,6 +3,9 @@
 #[cfg(feature = "av-nvidia")]
 use cudarc::driver::CudaDevice;
 
+#[cfg(feature = "av-nvidia")]
+use letsplay_av_ffmpeg::cuda_gl::safe::GraphicsResource;
+
 use letsplay_gpu::egl_helpers::DeviceContext;
 
 use std::sync::{Arc, Mutex};
@@ -11,6 +14,7 @@ use std::sync::{Arc, Mutex};
 pub struct GraphicsContexts {
 	pub egl_device_context: Arc<Mutex<DeviceContext>>,
 	pub cuda_context: Arc<CudaDevice>,
+	pub cuda_interop_context: Arc<Mutex<GraphicsResource>>,
 }
 
 #[cfg(not(feature = "av-nvidia"))]
@@ -24,9 +28,13 @@ impl GraphicsContexts {
 		let contexts = {
 			#[cfg(feature = "av-nvidia")]
 			{
+				let cuda_context = CudaDevice::new(gpu_index).expect("???");
 				GraphicsContexts {
-					cuda_context: CudaDevice::new(gpu_index).expect("???"),
+					cuda_context: cuda_context.clone(),
 					egl_device_context: Arc::new(Mutex::new(DeviceContext::new(gpu_index))),
+					cuda_interop_context: Arc::new(Mutex::new(GraphicsResource::new(
+						&cuda_context.clone(),
+					))),
 				}
 			}
 
