ad6a6d134ba2971b301265c6f92c1691fa615e3c
modeco80 <lily.modeco80@protonmail.ch>
2025-02-01 04:21:17 -0500

    letsplay_runner_core: Start to implement transport stuff

--- crates/letsplay_core/src/si_unit.rs
+++ crates/letsplay_core/src/si_unit.rs
@@ -1,5 +1,27 @@
 //! Because I'm big stupid with SI units
 
+/// Implements named SI unit types.
+macro_rules! implement_si_unit {
+	($name:ident, $s:ident, $calc:expr) => {
+		pub struct $name(pub usize);
+
+		impl $name {
+			/// Converts this unit to bytes.
+			pub const fn in_bytes(&$s) -> usize {
+				$calc
+			}
+		}
+	};
+}
+
+implement_si_unit!(Kb, self, self.0 * 125);
+implement_si_unit!(KiB, self, self.0 * 1024);
+implement_si_unit!(KB, self, self.0 * 1000);
+
+implement_si_unit!(MbNew, self, (self.0 / 8) * 1000000);
+implement_si_unit!(MBNew, self, self.0 * (1000 * 1000));
+implement_si_unit!(MiB, self, self.0 * (1024 * 1024));
+
 /// Megabit
 pub struct Mb(usize);
 
--- crates/letsplay_runner_core/src/shared/transport.rs
+++ crates/letsplay_runner_core/src/shared/transport.rs
@@ -1,15 +1,44 @@
-//
-// struct Transport<RW>
-// where
-//	RW: AsyncRead + AsyncWrite + ...
-//
-// - generic over literally anything that can be read or wtitten as a stream
-//
-// Use tokio util length delimited codec
-// https://docs.rs/tokio-util/latest/tokio_util/codec/length_delimited/index.html
-//
-// Have two helpers to pull out/parse protos of the each end's roots
-// (or fail cleanly)
-//
-// guess if we *really* wanted to (I don't see it being needed since we'll only own it in
-// a fairly flat way) we could have ReadTransport<R> and WriteTransport<W> but idk
+use letsplay_core::si_unit::MBNew;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
+
+use crate::shared::proto::{ClientMessage, ServerMessage};
+
+use futures_util::{SinkExt, StreamExt};
+
+// FIXME: DO NOT USE ANYHOW! DO NOT! SIMPLY DO NOT
+
+/// The max frame size of a Let's Play RPC message; in this case 4 MB.
+/// This may be lowered or bumped up; do not directly depend on this being stable (for now).
+pub const MAX_FRAME_SIZE: usize = MBNew(4).in_bytes();
+
+struct Transport<RW>
+where
+	RW: AsyncReadExt + AsyncWriteExt + Unpin,
+{
+	transport: Framed<RW, LengthDelimitedCodec>,
+}
+
+impl<RW> Transport<RW>
+where
+	RW: AsyncReadExt + AsyncWriteExt + Unpin,
+{
+	pub fn from_stream(stream: RW) -> Self {
+		Self {
+			transport: LengthDelimitedCodec::builder()
+				.length_field_type::<u32>()
+				.max_frame_length(MAX_FRAME_SIZE)
+				.new_framed(stream),
+		}
+	}
+
+	#[cfg(feature = "client")]
+	pub async fn read_server_message(&mut self) -> anyhow::Result<ServerMessage> {
+		if let Some(framed) = self.transport.next().await {
+			let bytes = framed?;
+			Ok(ServerMessage::parse(&bytes[..])?)
+		} else {
+			Err(anyhow::anyhow!("End of stream"))
+		}
+	}
+}
